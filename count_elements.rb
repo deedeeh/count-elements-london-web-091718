7db57a3882c7d30aafc7679a23180c02c1e469e5
deedeeh <dinahafez.8@gmail.com>
2018-09-24 10:38:15 +0100

    Done

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,12 @@
 def count_elements(array)
   # code goes here
+  animals_hash = {}
+  array.each do |animal|
+    if animals_hash.include?(animal)
+      animals_hash[animal] += 1
+    else
+      animals_hash[animal] = 1
+    end
+  end
+  animals_hash
 end
- 
